35fa2badeb7691fb1619aa9ecd0f9ddb2a22ab98
JesusOyola <jesus.oyola@interatica.com>
2025-11-10 12:50:29 -0300

    feat: routes configuration

---
+++ src/app/components/cocktail-search/cocktail-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktail-search',
+  imports: [],
+  templateUrl: './cocktail-search.component.html',
+  styleUrl: './cocktail-search.component.scss'
+})
+export class CocktailSearchComponent {
+
+}
